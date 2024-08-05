4b43bab5bc613d9331bd0fd9351150af7b6a1b3c
GvN1989 <geesje.noordenburg@gmail.com>
2024-08-05 11:52:42 +0200

    H6 opdracht uitgewerkt

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,19 @@
 public class Main {
 
     public static void main(String[] args) {
-        String[] countdown  = {"one","two","three"};
+        String[] countdown  = {"one","two","three", "boom!"};
         String[] names = {"Sjaak", "Sjimmy", "Sjonny"};
 
         // implementeer de countdown met een array three...two...one... boom!
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println("boom!");
+        System.out.println(countdown[2]);
+        System.out.println(countdown [1]);
+        System.out.println(countdown[0]);
+        System.out.println(countdown [3]);
 
         // implementeer een for loop die alle namen in de array "names" print. Houd er rekening mee dat de array een andere length kan hebben dan 3.
-        //for(;;) {
+        for (String s : names) {
+            System.out.println(s);
+        }//for(;;) {
 
         //}
 
@@ -20,9 +22,9 @@ public class Main {
         // twoSjimmy
         // threeSjonny
 
-        //for(;;) {
-
-        //}
+        for (int i = 0; i < countdown.length && i < names.length; i++) {
+            System.out.println(countdown[i] + names[i]);
+        }
     }
 
 }
